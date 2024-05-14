cda7679cb406d3591f9fa4d83f980306a9b5eb98
HungStark <94422669+HungStark@users.noreply.github.com>
2024-05-14 22:57:36 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -25,26 +25,13 @@ function getDirectionalLights(intensity) {
     }
 }
 
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.05, 5 * Math.pow(10,13));
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5 * Math.pow(10,8));
 setCamera(camera);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-
-function createPlanet(radius, src, distance, speed, rotation, cloud = false, ring = false)  {
-    var texture = getTexture(src);
-    var geometry = new THREE.SphereGeometry(radius, 32, 32);
-    var material = new THREE.MeshPhongMaterial({
-
-    });
-    var planet = new THREE.Mesh(geometry, material);
-    planet.distance = distance;
-    planet.speed = speed;
-    return planet;
-}
 
 function getTexture(src) { 
     var textureLoader = new THREE.TextureLoader();
@@ -58,20 +45,34 @@ function createSun() {
     var texture = getTexture('/textures/sun_detailed.png');
     texture.minFilter = THREE.NearestFilter;
 
-    var geometry = new THREE.SphereGeometry(20, 84, 42);
+    var geometry = new THREE.SphereGeometry(10, 64, 42);
 
     var material = new THREE.MeshBasicMaterial({
-        map: texture,
-        mapLight: texture,
-        flatShading: false
+        map: texture
     });
     
     var sun = new THREE.Mesh(geometry, material);
 
-    var sunLight = new THREE.PointLight(0xffffff, 2.0, 1000000, 0.6);
+    var sunLight = new THREE.PointLight(0xffffff, 5.0, 1000000, 0.01);
 
     sun.add(sunLight);
     return sun;
+}
+
+const time = new Date().getTime();
+
+function createPlanet(radius, src, distanceFromParent, cloud = false, ring = false)  {
+    var texture = getTexture(src);
+    var geometry = new THREE.SphereGeometry(radius, 32, 32);
+    var material = new THREE.MeshPhongMaterial({
+        map: texture,
+        flatShading: false
+    });
+    var planet = new THREE.Mesh(geometry, material);
+    planet.distanceFromParent = distanceFromParent;
+
+    planet.position.set(Math.cos(2.5) * planet.distanceFromParent, 0, Math.sin(2.5) * planet.distanceFromParent);
+    return planet;
 }
 
 function update(renderer, scene, camera, controls) {
@@ -85,10 +86,11 @@ function update(renderer, scene, camera, controls) {
 
 var controls = new OrbitControls( camera, renderer.domElement);
 
-var planets = []
+var earth = createPlanet(2, "textures/earth_10k.jpg", 140);
 var sun = createSun();
 
 scene.add(sun);
+scene.add(earth);
 scene.add(controls);
 var intensity = 2.0;
 //getDirectionalLights(intensity);
